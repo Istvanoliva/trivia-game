23f6ed7331f0d97c09a6e8800fd2aec175a20fbf
HenriqueSerres <henriqueserres@gmail.com>
2022-02-15 11:00:43 -0300

    req 2 parcial grupo 6

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import React from 'react';
 import Login from './page/Login';
+import Jogo from './page/Jogo';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/jogo" component={ Jogo } />
       </Switch>
     );
   }
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { loginName, loginEmail } from '../redux/actions';
+import tokenRedux from '../redux/reducers/token';
+
+const URL = 'https://opentdb.com/api_token.php?command=request';
 
 class Login extends React.Component {
   constructor() {
@@ -9,10 +13,18 @@ class Login extends React.Component {
       nome: '',
       email: '',
       disabled: true,
+      token: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.validateButton = this.validateButton.bind(this);
+  }
+
+  fetchAPI = async () => {
+    const fetchURL = await fetch(URL);
+    const response = await fetchURL.json();
+    const { token } = response;
+    this.setState({ token });
   }
 
   validateButton() {
@@ -25,7 +37,10 @@ class Login extends React.Component {
   }
 
   handleClick() {
-    console.log('clicou');
+    const { history, setTokenDispatch } = this.props;
+    const { token } = this.state;
+    setTokenDispatch(token);
+    history.push('/jogo');
   }
 
   render() {
@@ -66,8 +81,15 @@ class Login extends React.Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.string,
+  setTokenDispatch: PropTypes.func,
+}.isRequired;
+
 const mapDispatchToProps = (dispatch) => ({
   loginNameDispatch: (nome) => dispatch(loginName(nome)),
   loginEmailDispatch: (email) => dispatch(loginEmail(email)),
+  setTokenDispatch: (token) => dispatch(tokenRedux(token)),
 });
 export default connect(null, mapDispatchToProps)(Login);
