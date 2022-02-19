b6379da8496208ed13e96c0323bd6692c0b904c4
HenriqueSerres <henriqueserres@gmail.com>
2022-02-19 18:07:43 -0300

    req 8 e 9 sala 6

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import loginRedux from './login';
 import token from './token';
 import player from './player';
+import timeDown from './timer';
 
 const rootReducer = combineReducers({
   loginRedux,
   token,
   player,
+  timeDown,
 });
 
 export default rootReducer;
